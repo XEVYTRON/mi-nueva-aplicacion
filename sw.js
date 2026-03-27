d38cc56a2d5da817b3084630145ecab8c94fda00
XEVYTRON <VICTORMANUEL300@HOTMAIL.COM>
2026-03-27 22:09:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-const CACHE_NAME = 'puntos-cloud-v18';
-const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { 
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); 
+    self.clients.claim(); 
 });
-
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    )));
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
